72119d95bcb96e64bd9f7e8da08e518c27d2b7bf
Oliver Klee <typo3-coding@oliverklee.de>
2026-09-15 16:44:51 +0200

    [TASK] Update the DDEV-generated `additional.php` (#834)

--- config/system/additional.php
+++ config/system/additional.php
@@ -7,38 +7,48 @@
  */
 
 if (getenv('IS_DDEV_PROJECT') == 'true') {
-    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
-        $GLOBALS['TYPO3_CONF_VARS'],
-        [
-            'DB' => [
-                'Connections' => [
-                    'Default' => [
-                        'dbname' => 'db',
-                        'driver' => 'mysqli',
-                        'host' => 'db',
-                        'password' => 'db',
-                        'port' => 3306,
-                        'user' => 'db',
-                    ],
+    $ddevConfig = [
+        // This GFX configuration allows processing by installed ImageMagick 6
+        'GFX' => [
+            'processor' => 'ImageMagick',
+            'processor_path' => '/usr/bin/',
+            'processor_path_lzw' => '/usr/bin/',
+        ],
+        // This mail configuration sends all emails to mailpit
+        'MAIL' => [
+            'transport' => 'smtp',
+            'transport_smtp_encrypt' => false,
+            'transport_smtp_server' => 'localhost:1025',
+        ],
+        'SYS' => [
+            'trustedHostsPattern' => '.*.*',
+            'devIPmask' => '*',
+            'displayErrors' => 1,
+        ],
+    ];
+
+    // Only override the database connection if the project uses a driver
+    // provided by the DDEV db container. If the Default connection has been
+    // configured for anything else (like SQLite), leave it alone.
+    $ddevDriver = $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['driver'] ?? 'mysqli';
+    if (in_array($ddevDriver, ['mysqli', 'pdo_mysql', 'pdo_pgsql'], true)) {
+        $ddevConfig['DB'] = [
+            'Connections' => [
+                'Default' => [
+                    'dbname' => 'db',
+                    'driver' => 'mysqli',
+                    'host' => 'db',
+                    'password' => 'db',
+                    'port' => 3306,
+                    'user' => 'db',
                 ],
             ],
-            // This GFX configuration allows processing by installed ImageMagick 6
-            'GFX' => [
-                'processor' => 'ImageMagick',
-                'processor_path' => '/usr/bin/',
-                'processor_path_lzw' => '/usr/bin/',
-            ],
-            // This mail configuration sends all emails to mailpit
-            'MAIL' => [
-                'transport' => 'smtp',
-                'transport_smtp_encrypt' => false,
-                'transport_smtp_server' => 'localhost:1025',
-            ],
-            'SYS' => [
-                'trustedHostsPattern' => '.*.*',
-                'devIPmask' => '*',
-                'displayErrors' => 1,
-            ],
-        ]
+        ];
+    }
+
+    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
+        $GLOBALS['TYPO3_CONF_VARS'],
+        $ddevConfig
     );
+    unset($ddevConfig, $ddevDriver);
 }
